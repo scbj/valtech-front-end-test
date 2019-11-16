be795f7fd0cd1985b38a01e1b530a09f8d263da6
Sacha <contact@scbj.io>
2019-11-16 22:15:26 +0100

    <NavigationList> and images importing
    ◽ Create and implement Navigation List component
    ◽ Add jsconfig.json to handle relative path'@'
    ◽ Import images

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavigationList />
     <router-view/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavigationList from '@/components/NavigationList.vue'
+
+export default {
+  components: {
+    NavigationList
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,15 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/NavigationList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="navigation-list">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.navigation-list {
+  padding: 30px;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+
+  &.router-link-exact-active {
+    color: #42b983;
+  }
+}
+</style>
