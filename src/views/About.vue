8288b34c04fe149a9fc291220558f4a0789fd5c3
Sacha <contact@scbj.io>
2019-11-18 03:17:02 +0100

    Write about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>Front end test :</h3>
+    <p>
+      You have to implement <a href="/docs/testfront.pdf" target="_blank">this wireframe</a> thanks to any technology that you want. It's a fluid content. You should also work on the mobile version of the website.
+      <br>For the hover, you can give us another way to do it, you are totally free for this part, we just suggest a standard case.
+      <br>The article contain a link to go to a page of your choice.
+    </p>
+    <p>
+      We will have a look to your code, to the SEO and the accessibility.
+    </p>
+    <h3>Javascript :</h3>
+    <p>
+      You need to create a modal box when you clic on a picture. The content of this modal is the background picture + the title of the box you clic. Design is up to you.
+    </p>
+    <p>
+      Good luck
+    </p>
   </div>
 </template>
+
+<style scoped lang="scss">
+.about {
+  text-align: left;
+
+  max-width: 40rem;
+  padding: 1rem;
+  margin: auto;
+}
+
+h3 {
+  font-size: 1.8rem;
+  font-weight: 400;
+  margin-top: 2em;
+}
+
+p {
+  margin-top: 1.4em;
+  line-height: 2;
+}
+
+a {
+  color: #d40000;
+}
+</style>
